4b280eed1b12f94332021f14bf6044a3214c038a
Carson Rajcan <carajcan@gmail.com>
2023-07-20 09:26:35 -0500

    1 hour order book enhancement

--- src/interview_questions/order_book.rs
+++ src/interview_questions/order_book.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
 use std::collections::BTreeMap;
+use std::collections::VecDeque;
 
+#[derive(Clone)]
 enum Side {
     Buy,
     Sell
 }
 
+#[derive(Clone)]
 struct Order {
   id: usize,
   price: i32,
@@ -17,6 +20,7 @@ struct Order {
 struct PriceLevel {
     size: usize,
     price: i32,
+    order_ids: VecDeque<usize>,
 }
 
 struct OrderBook {
@@ -24,6 +28,12 @@ struct OrderBook {
     buy_price_levels: BTreeMap<i32, PriceLevel>,
     sell_orders: HashMap<usize, Order>,
     sell_price_levels: BTreeMap<i32, PriceLevel>,
+}
+
+struct Trade {
+    bought: Vec<Order>,
+    sold: Vec<Order>,
+    remainder: Option<Order>
 }
 
 impl OrderBook {
@@ -36,23 +46,104 @@ impl OrderBook {
         }
     }
 
-    fn add_order(&mut self, order: Order) {
+    fn trade(&mut self, other_order: usize, order: Order) -> (Order, Order) {
+         
+    }
+
+
+    fn try_trade(&mut self, mut order: Order) -> Trade {
+        let mut bought = vec![];
+        let mut sold = vec![];
+
+        let order = match order.side {
+            Side::Buy => {
+                
+                loop {
+                    let best_ask = self.best_ask();
+                    if best_ask.is_none() {
+                        break;
+                    }
+
+                    if best_ask.unwrap().price <= order.price {
+                        break;
+                    }
+
+                    match self.sell_price_levels.get_mut(&order.price) {
+                        Some(price_level) => {
+                            let to_trade = price_level.order_ids.pop_front();
+
+                            match to_trade {
+                                None => break,
+                                Some(other_order_id) => {
+                                    (order, other_trade) = self.trade(other_order_id, order);
+                                } 
+                            }
+                        },
+                        None => break,
+                    }
+                    
+                        
+                }
+
+                order
+            },
+            Side::Sell => {
+
+            },
+        };
+
+        Trade {
+            bought,
+            sold,
+            remainder: order,
+        }
+    }
+
+    fn add_order(&mut self, order: Order) -> Trade { 
+        let trade = self.try_trade(order);
+
+        match trade.remainder {
+            Some(order) => self.insert_order_to_book(order.clone()),
+            None => (),
+        }
+
+        trade 
+    }
+
+    fn insert_order_to_book(&mut self, order: Order) {
         let order_size = order.size;
         let order_price = order.price;
+        let order_id = order.id;
 
         match order.side {
             Side::Buy => {
                 self.buy_orders.insert(order.id, order);
 
                 self.buy_price_levels.entry(order_price)
-                    .and_modify(|price_level| price_level.size += order_size)
-                    .or_insert(PriceLevel { size: order_size, price: order_price });
+                    .and_modify(|price_level| {
+                        price_level.size += order_size;
+                        price_level.order_ids.push_back(order_id);
+                    })
+                    .or_insert({
+                        let mut order_ids = VecDeque::new();
+                        order_ids.push_back(order_id);
+
+                        PriceLevel { size: order_size, price: order_price, order_ids }
+                    });
             },
             Side::Sell => {
                 self.sell_orders.insert(order.id, order);
                 self.sell_price_levels.entry(order_price)
-                    .and_modify(|price_level| price_level.size += order_size)
-                    .or_insert(PriceLevel { size: order_size, price: order_price });
+                    .and_modify(|price_level| {
+                        price_level.size += order_size;
+                        price_level.order_ids.push_back(order_id);
+                    })
+                    .or_insert({
+                        let mut order_ids =  VecDeque::new();
+                        order_ids.push_back(order_id);
+
+                        PriceLevel { size: order_size, price: order_price, order_ids }
+                    });
             },
         }
     }
@@ -124,8 +215,10 @@ mod test {
 
         // book.delete_order(4);
 
-        assert_eq!(book.best_bid(), Some(PriceLevel { price: 99, size: 4 }));
-        assert_eq!(book.best_ask(), Some(PriceLevel { price: 100, size: 3 }));
+        assert_eq!(book.best_bid().unwrap().price, 99);
+        assert_eq!(book.best_bid().unwrap().size, 4);
+        assert_eq!(book.best_ask().unwrap().price, 100);
+        assert_eq!(book.best_ask().unwrap().size, 3);
         println!("tests passed");
     }
 }
