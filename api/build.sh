a36d0070b960209150813c263d7cf12594202a30
Carson Rajcan <carajcan@gmail.com>
2023-09-11 17:49:44 -0500

    Retry build.sh

---
+++ api/build.sh
@@ -0,0 +1,5 @@
+curl -sLO https://github.com/protocolbuffers/protobuf/releases/download/v22.2/protoc-22.2-linux-x86_64.zip
+unzip protoc-22.2-linux-x86_64.zip -d $HOME/.local
+export PATH="$PATH:$HOME/.local/bin"
+
+cargo install --git https://github.com/getdozer/dozer dozer-cli --locked
