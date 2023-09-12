f5e04877237d283ca6c97f07b56e8c785d7225f1
Carson Rajcan <carajcan@gmail.com>
2023-09-12 09:39:44 -0500

    Add Dockerfile

--- api/build.sh
+++ api/build.sh
@@ -2,4 +2,5 @@ curl -sLO https://github.com/protocolbuffers/protobuf/releases/download/v22.2/pr
 unzip protoc-22.2-linux-x86_64.zip -d $HOME/.local
 export PATH="$PATH:$HOME/.local/bin"
 
-cargo install --git https://github.com/getdozer/dozer dozer-cli
+curl -sLO https://github.com/getdozer/dozer/releases/latest/download/dozer-linux-amd64.deb
+sudo dpkg -i dozer-linux-amd64.deb
