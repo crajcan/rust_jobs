b7912bd5448b1516644cca9f7b5f212cbc4b2c3c
Carson Rajcan <carajcan@gmail.com>
2023-09-14 16:16:43 -0500

    Pin down dozer version

--- api/build.sh
+++ api/build.sh
@@ -6,10 +6,10 @@ curl -sLO https://github.com/protocolbuffers/protobuf/releases/download/v22.2/pr
 unzip protoc-22.2-linux-x86_64.zip -d $HOME/.local 
 
 # Uncomment for ARM based build
-# curl -sLO https://github.com/getdozer/dozer/releases/latest/download/dozer-linux-aarch64.deb
+# curl -sLO https://github.com/getdozer/dozer/releases/download/v0.1.37/dozer-linux-aarch64.deb
 # dpkg -i dozer-linux-aarch64.deb
 
-curl -sLO https://github.com/getdozer/dozer/releases/latest/download/dozer-linux-amd64.deb
+curl -sLO https://github.com/getdozer/dozer/releases/download/v0.1.37/dozer-linux-amd64.deb
 dpkg -i dozer-linux-amd64.deb
 
 echo "replacing <pg_password> in dozer-config.yaml with PG_PASSWORD"
